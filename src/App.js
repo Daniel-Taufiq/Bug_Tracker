d35eb9332b5d25099bc236668784e411f8366a29
Daniel Taufiq <taufiqdanny@gmail.com>
2021-05-09 01:49:18 -0600

    added form and adjusted elements

--- src/App.js
+++ src/App.js
@@ -11,8 +11,17 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <h1>Bug Tracker</h1>
+                <h1 className={styles.title}>Bug Tracker</h1>
                 <div className={styles.box}></div>
+                <div className={styles.form}>
+                    <form action="/Account_Authentication.js">
+                        <label for="username">Username</label>
+                        <input type="text" id="username" name="Username" placeholder="Username.."></input>
+                        <label for="password">Password</label>
+                        <input type="text" id="password" name="Password" placeholder="Password.."></input>
+                        <input type="submit" value="Submit"></input>
+                    </form>
+                </div>
             </div>
         );
     }
