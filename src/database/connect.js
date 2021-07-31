38b6957627f0039b88296568a58c5b8dec6c3742
Daniel-Taufiq <taufiqdanny@gmail.com>
2021-07-31 09:52:30 -0600

    added signin and pasword hashing

--- src/database/connect.js
+++ src/database/connect.js
@@ -2,16 +2,74 @@ require('dotenv').config();
 const bodyParser = require('body-parser')
 const mysql = require('mysql');
 const express = require('express');
-const fetch = require('node-fetch');
+const bcrypt = require('bcrypt');
 const app = express();
 const password = process.env.PW;
+const saltRounds = 10;
 const port = process.env.PORT || 3000;
+let hash = '';
 
 app.use(bodyParser.urlencoded({
     extended: true
   }));
 
 app.use(express.json());
+
+// -------------------routes --------------------------
+
+app.post('/signup', function(req, res) {
+    let username = req.body.username;
+    let password = generatePasswordHash(req.body.password);
+
+
+    let insert_query = 'INSERT INTO users (username, password) VALUES ("' + username + '", "' + password + '")';
+
+    connection.query(insert_query, function(err, result) {
+        if (err) {
+            console.error('Error inserting into table', err);
+            return;
+        }
+        res.send('Success');
+    });
+});
+
+app.get('/signin', function(req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // get hash from database
+    let select_query = 'SELECT password FROM users WHERE username = "' + username + '"';
+    connection.query(select_query, function(err, result) {
+        if (err) {
+            console.error('Error selecting from table', err);
+            return;
+        }
+        if (result.length === 0) {
+            res.send('Username not found');
+        } else {
+            let hash = result[0].password;
+            if (bcrypt.compareSync(password, hash)) {
+                res.send('Success');
+            } else {
+                res.send('Incorrect password');
+            }
+        }
+    });
+});
+
+
+// -------------------helper functions --------------------
+
+function generatePasswordHash(password) {
+    return bcrypt.hashSync(password, saltRounds);
+}
+
+function checkPassword(password, hash) {
+    return bcrypt.compareSync(password, hash);
+}
+
+
+// -------------------database connection--------------------------
 
 let connection = mysql.createConnection({
     host: 'localhost',
@@ -43,20 +101,5 @@ function create_table() {
         console.log('Table created');
     });
 }
-
-app.post('/signup', function(req, res) {
-    let username = req.body.username;
-    let password = req.body.password;
-
-    let insert_query = 'INSERT INTO users (username, password) VALUES ("' + username + '", "' + password + '")';
-
-    connection.query(insert_query, function(err, result) {
-        if (err) {
-            console.error('Error inserting into table', err);
-            return;
-    }
-    res.send('Success');
-    });
-});
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
